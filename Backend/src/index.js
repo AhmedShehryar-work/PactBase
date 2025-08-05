b15e9613d91c4f8dc8b3c0801d6ba533ee153f8c
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-08-05 16:20:33 +0500

    Started Backend

---
+++ Backend/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  //TODO: connectDB();
+});
