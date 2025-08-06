9d95ebd6a2d93d00669faeb34286bf58a5c3fca1
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-08-06 23:55:59 +0500

    Added db.js to the config and configured database

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,1 +1,8 @@
-//connect to db here with pg or postgres
+// lib/db.js
+import postgres from 'postgres';
+
+const Q = postgres(process.env.DATABASE_URI, {
+  ssl: 'require' // required for Supabase
+});
+
+export default Q;
