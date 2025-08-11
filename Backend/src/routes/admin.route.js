43751f7a461f19280e5d09c76f61e876178e11f7
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-08-11 23:59:49 +0500

    Added the get User route to the routing

--- Backend/src/routes/admin.route.js
+++ Backend/src/routes/admin.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { activate, disable } from "../controllers/admin/status.controller.js";
+import { getUser, activate, disable } from "../controllers/admin/status.controller.js";
 
 const router = express.Router();
 
+router.post("/activate", getUser);
 router.post("/activate", activate);
 router.post("/disable", disable);
 
