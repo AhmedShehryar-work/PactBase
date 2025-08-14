802eb84e664fb43660c74acc5e9c0735d0416b89
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-08-14 21:36:59 +0500

    Added Login logic to the backend with token generation

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import Q from "../config/db.js"
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
 import { v4 as uid } from 'uuid';
 
 export const signup = async (req, res) => {
@@ -7,19 +9,48 @@ export const signup = async (req, res) => {
 
         const {fullName, username, email, password, cnicNo} = req.body;
 
+        const normalizedUsername = username.toLowerCase();
+        const normalizedEmail = email.toLowerCase();
+
+        const [existingUsername] = await Q`
+            SELECT id
+            FROM users
+            WHERE username = ${normalizedUsername}
+            LIMIT 1
+        `;
+
+        if (existingUsername) {
+        return res.status(400).json({ message: "Username already exists", status: "duplicate_username" });
+        }
+
+        // Check email
+        const [existingEmail] = await Q`
+            SELECT id
+            FROM users
+            WHERE email = ${normalizedEmail}
+            LIMIT 1
+        `;
+
+        if (existingEmail) {
+        return res.status(400).json({ message: "Email already exists", status: "duplicate_email" });
+        }
+
         const user_id = uid();
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         try {
             await Q.begin(async (sqlTx) => {
 
                 await sqlTx`
                 INSERT INTO users (id, full_name, username, email, password)
-                VALUES (${user_id}, ${fullName}, ${username}, ${email}, ${password})
+                VALUES (${user_id}, ${fullName}, ${normalizedUsername}, ${normalizedEmail}, ${hashedPassword})
                 `;
 
                 await sqlTx`
                 INSERT INTO pending_users (id, cnic_no)
-                VALUES (${cnicNo}, ${cnicNo})
+                VALUES (${user_id}, ${cnicNo})
                 `;
 
             });
@@ -42,7 +73,52 @@ export const login = async (req, res) =>{
 
     try{
         const {username, password} = req.body;
-        
+
+        const normalizedUsername = username.toLowerCase();
+
+        const [user] = await Q`
+            SELECT *
+            FROM users
+            WHERE username = ${normalizedUsername}
+            LIMIT 1
+        `;
+
+        if (!user) {
+        return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        switch (user.status) {
+            case "active":
+                // proceed with login
+                generateToken(user.id, res);
+                return res.status(200).json({
+                id: user.id,
+                fullName: user.full_name,
+                email: user.email,
+                profilePic: user.profile_image,
+                blockedUsers: user.blocked_users,
+                blockedBy: user.blocked_by,
+                rating: user.rating,
+                pacts_fulfilled: user.pacts_fulfilled,
+                created_at: user.created_at
+                });
+
+            case "disabled":
+                return res.status(403).json({ message: "Account is disabled. Contact support.", status: "disabled" });
+
+            case "pending":
+                return res.status(403).json({ message: "Account pending approval.", status: "pending" });
+
+            default:
+                return res.status(400).json({ message: "Invalid account status" });
+        }
 
     } catch (error) {
         console.log("ERROR: ", error);
