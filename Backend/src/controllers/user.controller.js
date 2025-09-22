f6528ec3356998fd6377e3c51548488372e34d26
Ahmed Shehryar <ahmedshehryar.work@gmail.com>
2025-09-22 14:20:44 +0500

    Added just a starter for password chnage

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -6,5 +6,10 @@ export const verifyEmail = async (req, res) =>{
 }
 
 export const changePassword = async (req, res) =>{
-
+    
+    // await Q`
+    // UPDATE users
+    // SET pacts_received = array_append(pacts_received, ${pactId})
+    // WHERE username = ANY(${to}::text[]);
+    // `;
 }
